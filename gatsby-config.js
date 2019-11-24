321cbe547c77272167b057581b6d097aa616f3b9
rebecca <rebeccahey1001@gmail.com>
2019-11-24 20:35:50 +0000

    adding sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,16 +3,11 @@ require('dotenv').config({
 });
 
 module.exports = {
-siteMetadata: {
-siteUrl: `https://www.brewplus.com`,
-},
-plugins: [`gatsby-plugin-sitemap`],
-},
-module.exports = {
   siteMetadata: {
     title: `Tanks, hygienic fittings and brewing equipment`,
     description: `High quality tanks, hygienic fittings and brewing equipment sourced by brewers for brewers.`,
-    author: `@gatsbyjs`
+    author: `@gatsbyjs`,
+    siteUrl: `https://www.brewplus.com`
   },
   plugins: [
     {
@@ -38,6 +33,9 @@ module.exports = {
       }
     },
     {
+       resolve: `gatsby-plugin-sitemap`
+  },
+    {
       resolve: `gatsby-source-prismic`,
       options: {
         repositoryName: process.env.PRISMIC_REPO_NAME
@@ -62,8 +60,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
-    }
-
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
