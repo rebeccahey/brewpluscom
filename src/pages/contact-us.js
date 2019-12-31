92c12ddded0ff42706e6734299f2ce1711b0fa87
rebeccahey <53094295+rebeccahey@users.noreply.github.com>
2019-12-31 10:25:41 +0000

    Update contact-us.js

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -10,7 +10,7 @@ const ContactUs = () => (
     <p css={{'@media (min-width: 100px)': {paddingLeft: "70px",
                 paddingRight: "70px"}}}>If you'd like to get a quote or have a question please send us an email or give us a call. <br /><br />
                   Email: <a href="mailto:hello@brewplus.com">hello@brewplus.com</a><br /> <br />
-                  Phone:  07939 447 382<br /> <br />
+                  Phone:  07882 090 659<br /> <br />
                   Address: 454 Long Lane, London, N2 8JL, UK <br /> <br />
                   Have a question? We'd love to hear from you!<br /><br /> </p>
   </>
